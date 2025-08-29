dc97e1257741b2d89feb3cb001413d30df200161
zoynt <dhoniznr31@gmail.com>
2025-08-29 22:22:17 +0800

    Merge branch && fix sidebar

--- resources/views/layouts/partials/_tps_table_body.blade.php
+++ resources/views/layouts/partials/_tps_table_body.blade.php
@@ -5,7 +5,11 @@
         <td class="px-6 py-4 font-semibold text-gray-800">{{ $schedule->tps_name }}</td>
         <td class="px-6 py-4">{{ $schedule->tps_address }}</td>
         <td class="px-6 py-4">{{ $schedule->kecamatan }}</td>
-        <td class="px-6 py-4">{{ $schedule->tps_day }}</td>
+        <td class="px-6 py-4">
+            @if(is_array($schedule->tps_day))
+                {{ implode(', ', $schedule->tps_day) }}
+            @endif
+        </td>
         <td class="px-6 py-4">{{ date('H:i', strtotime($schedule->tps_start_time)) }} -
             {{ date('H:i', strtotime($schedule->tps_end_time)) }} WITA</td>
         <td class="px-6 py-4">{{ $schedule->tps_transport }}</td>
