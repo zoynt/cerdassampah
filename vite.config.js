af50bbcef11560dfe1a9517c406afd88bf0226b0
zoynt <dhoniznr31@gmail.com>
2025-09-08 10:14:33 +0800

    fix game

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
     server: {
-        https: false,
+        https: true,
         host: true, // <-- UBAH BARIS INI
     },
     plugins: [
