389b596b622bd48b4781a9ca20889d1f6fadcf6d
zoynt <dhoniznr31@gmail.com>
2025-09-15 15:09:27 +0800

    [Merge] add FE Bank Sampah Digital

--- app/Models/UserPoint.php
+++ app/Models/UserPoint.php
@@ -1,10 +1,37 @@
 <?php
 
+// app/Models/UserPoint.php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class UserPoint extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Nama tabel yang terhubung dengan model.
+     *
+     * @var string
+     */
+    protected $table = 'user_points';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'points',
+    ];
+
+    /**
+     * Relasi ke model User.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
