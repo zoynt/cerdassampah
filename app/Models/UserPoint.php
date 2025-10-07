47945c0753b639f75d545aeec1f56faf686f1fbc
zoynt <dhoniznr31@gmail.com>
2025-10-07 14:31:02 +0800

    Fix UserPoint

---
+++ app/Models/UserPoint.php
@@ -0,0 +1,38 @@
+<?php
+
+// app/Models/UserPoint.php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPoint extends Model
+{
+    use HasFactory;
+
+    /**
+     * Nama tabel yang terhubung dengan model.
+     *
+     * @var string
+     */
+    protected $table = 'user_points';
+     protected $primaryKey = 'user_points_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'points',
+    ];
+
+    /**
+     * Relasi ke model User.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
