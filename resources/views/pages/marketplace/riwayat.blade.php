f9a8422710904f8898b68cc2d7f39f548cb16d65
anissanr274 <anissanr020704@gmail.com>
2025-12-28 21:00:09 +0800

    button data penjualan

--- resources/views/pages/marketplace/riwayat.blade.php
+++ resources/views/pages/marketplace/riwayat.blade.php
@@ -117,7 +117,7 @@
                     </div>
                 </div>
 
-                <div class="flex gap-4 w-full md:w-auto">
+                <div class="flex flex-col md:flex-row gap-4 w-full md:w-auto">
 
                     <a href="{{ route('marketplace.riwayat.export') }}"
                         class="inline-flex items-center justify-center px-4 py-2.5 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 text-sm w-full md:w-auto">
